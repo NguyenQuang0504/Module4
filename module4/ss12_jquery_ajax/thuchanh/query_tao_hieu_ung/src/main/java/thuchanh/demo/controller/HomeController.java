41a9a9fcf511d3ec87a6f7ef57ad362a3bfcfc2a
NguyenQuang0504 <quang10a30504@gmail.com>
2022-03-19 20:23:39 +0700

    up bt

---
+++ module4/ss12_jquery_ajax/thuchanh/query_tao_hieu_ung/src/main/java/thuchanh/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package thuchanh.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+    @GetMapping("/home2")
+    public String home2(){
+        return "home2";
+    }
+}
